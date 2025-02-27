15961463148881393ff49b8885264822a71de1bc
CUBSPLOSIVE <iangonzalez@isu.edu>
2025-02-27 15:07:29 -0700

    Initial Database COMMIT

--- CoordsApp/app/src/main/java/com/example/coordsapp/MainActivity.kt
+++ CoordsApp/app/src/main/java/com/example/coordsapp/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.room.Room
+import com.example.coordsapp.db.LocationDatabase
 
 
 // You should see this message if you updated/pulled correctly
 class MainActivity : AppCompatActivity() {
+    val db = Room.databaseBuilder(applicationContext, LocationDatabase::class.java, "coords_database").build()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
