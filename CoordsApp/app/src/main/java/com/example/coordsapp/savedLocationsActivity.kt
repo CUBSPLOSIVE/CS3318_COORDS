f5cd2e38667c6fb6d5bad6a18fb91860a0cbdd47
dominikthompson <dominikthompson@isu.edu>
2025-03-10 13:16:06 -0600

    Front page UI Start. -Added a navigation arrow -Added variables for the coordinates on the front page -Added Saved Locations -Added Location 1 form

--- CoordsApp/app/src/main/java/com/example/coordsapp/savedLocationsActivity.kt
+++ CoordsApp/app/src/main/java/com/example/coordsapp/savedLocationsActivity.kt
@@ -1,6 +1,8 @@
 package com.example.coordsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,20 @@ class savedLocationsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_saved_locations)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnLocationOne = findViewById<Button>(R.id.btnLocationOne)
+
+        btnLocationOne.setOnClickListener{
+            val intent = Intent(this,Location1::class.java)
+            startActivity(intent)
         }
+
+//        val btnSavedActivity = findViewById<Button>(R.id.btnSaved)
+//
+//        btnSavedActivity.setOnClickListener{
+//            val intent = Intent(this,savedLocationsActivity::class.java)
+//            startActivity(intent)
+//        }
+
     }
 }
